1d2fd5e35bd03e4852b641b8f33f8383edc90c30
Bruno Sponton <brunogsponton@hotmail.com>
2024-09-23 20:35:21 -0300

    Agregar archivos de proyecto.

---
+++ ValidacionPatente/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ValidacionPatente
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string patente = textBox1.Text;
+
+            // Para verificar formato "AANNNAA" o "AAANNN"
+            if (patente.Length == 7 && char.IsLetter(patente[0]) && char.IsLetter(patente[1]) && char.IsDigit(patente[2]) &&
+                char.IsDigit(patente[3]) && char.IsDigit(patente[4]) && char.IsLetter(patente[5]) && char.IsLetter(patente[6]))
+            {
+                MessageBox.Show("La patente es válida con formato 'AANNNAA'.", "Éxito");
+            }
+            else if (patente.Length == 6 && char.IsLetter(patente[0]) && char.IsLetter(patente[1]) && char.IsLetter(patente[2]) &&
+                     char.IsDigit(patente[3]) && char.IsDigit(patente[4]) && char.IsDigit(patente[5]))
+            {
+                MessageBox.Show("La patente es válida con formato 'AAANNN'.", "Éxito");
+            }
+            else
+            {
+                MessageBox.Show("Formato de patente inválido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Para permitir solo letras y números
+            if (!char.IsLetterOrDigit(e.KeyChar) && e.KeyChar != (char)Keys.Back)
+            {
+                MessageBox.Show("Solo se permiten letras y números.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
